2440c14996c3ccf63456adfdf97dd3f792062404
Jimin Huh <jiminhuh95@gmail.com>
2019-03-19 14:42:46 -0500

    finish connecting fs writeFile to friends.js and post method for client data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,1 @@
-var apiRoutes = require("../routing/apiRoutes");
+[{"name":"Helen","photo":"https://google.com","scores":[5,1,2,4,4,5,1,2,5,4,1]}]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,24 +1,38 @@
 const fs = require("fs");
+const path = require("path");
 
 module.exports = (app) => {
     let friends = [];
 
+    // Post , data from survey
     app.post("/api/friends", (req, res) => {
+        // Grab data from client-side
         var newFriend = req.body;
+        // adding to array to start, if there in no data inside array, add to friends.js file
         if(friends.length === 0) {
             friends.push(newFriend);
             res.send(friends);
-        } else {
-            friends.forEach((friend) => {
-                if(friend.name === newFriend.name && friends.photo === newFriend.photo) {
-                    res.send(`Hello ${newFriend.name}, Please use a different name & photo for your new profile. `);
-                } else {
-                    friends.push(newFriend);
-                    res.send(friends);
-                }
-            }) 
+            // Update friends.js file 
+            fs.writeFile(path.join(__dirname, "../data/friends.js"), JSON.stringify(friends), (err) => {
+                if(err) throw err;
+                console.log("friends.js updated");
+            })
+        // Array Filter to see if there are any data that has the exact same name and photo (duplicate entries)
+        } else if (friends.filter(e => (e.name === newFriend.name && e.photo === newFriend.photo)).length > 0) {
+                res.send(`Hello ${newFriend.name}, Please use a different name & photo for your new profile. `);
+            // not a duplicate -> add to array & write to friends.js file
+            } else {
+                friends.push(newFriend);
+                    // Update friends.js file 
+                fs.writeFile(path.join(__dirname, "../data/friends.js"), JSON.stringify(friends), (err) => {
+                    if(err) throw err;
+                    console.log("Updated!")
+                })
+                res.send(friends);
+                
+            }
         }
-    });
+    );
 
 
     app.get("/api/friends", (req, res) => {
